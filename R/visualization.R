23e51d0ae2a156f6474fc96d1a44b6ccd09cf438
Kevin Proudler <57147062+kproudler@users.noreply.github.com>
2023-11-25 11:29:49 -0500

    Update visualization.R

--- R/visualization.R
+++ R/visualization.R
@@ -1,1 +1,9 @@
-test
+install.packages("ggplot2")
+install.packages("palmerpenguins")
+library(ggplot2)
+library(palmerpenguins)
+
+data(penguins)
+View(penguins)
+
+ggplot(data=penguins)+geom_point(mapping = aes(x=flipper_length_mm, y=body_mass_g))
