b2ab56d0908123d8711cedba65062147c092d51e
Kevin Proudler <57147062+kproudler@users.noreply.github.com>
2023-11-25 20:37:33 -0500

    Update penguin_visualization.R

--- R/penguin_visualization.R
+++ R/penguin_visualization.R
@@ -9,3 +9,14 @@ ggplot(data=penguins)+geom_point(mapping = aes(x=bill_length_mm, y=bill_depth_mm
 ggplot(data=penguins) +
   geom_point(mapping=aes(x=flipper_length_mm, y=body_mass_g)) +
   geom_smooth(mapping=aes(x=flipper_length_mm, y=body_mass_g))
+
+ggplot(data=penguins)+
+  geom_smooth(mapping=aes(x=flipper_length_mm, y=body_mass_g, linetype=species))
+
+ggplot(data=penguins)+
+  geom_smooth(mapping=aes(x=flipper_length_mm, y=body_mass_g, linetype=species))
+  geom_jitter(mapping=aes(x=flipper_length_mm,y=body_mass_g))
+
+ggplot(data=penguins, aes(x=flipper_length_mm, y=body_mass_g))+
+  geom_point(aes(color=species))+
+  facet_grid(~sex)
